57989b0c8d083bc2a7013a7d8b5057cf476b6344
Neyxium <50521342+neyxium@users.noreply.github.com>
2022-11-05 00:05:52 +0100

    Update read.py
    add comments
    level count for each level count

--- read.py
+++ read.py
@@ -4,6 +4,7 @@ from pickle import TRUE
 from re import T
 from collections import Counter
 import time
+
   
 # Opening JSON file
 f = open('data.json')
@@ -25,14 +26,44 @@ for level in levels['levels']:
     userLevelCounter[userid] += 1
 
 
+
 # Sorting the dictionary
 sorted_dict = sorted(userLevelCounter.items(), key=lambda kv: kv[1], reverse=False)
+
+a = dict(Counter(userLevelCounter.values()))
+
+a = sorted(a.items(), key=lambda kv: kv[0], reverse=False)
 
 total_player = 0
 total_maps = 0
 
 # Printing the results
 for key, value in dict(sorted_dict).items():
+    total_player += 1
+    total_maps += value
+    file = open('logs.txt', 'a+')
+    file.write(str(userid_to_username[key]) + ' : ' + str(value) + "\n")
+    file.close()
+
+
+for levelcount, frequency in a:
+    print(levelcount, frequency)
+
+
+# Typing the total amount of players and maps in file
+
+file = open('logs.txt', 'a+')
+
+file.write("______________________________________" + "\n")
+file.write("total players with level editor " + str(total_player) + "\n")
+file.write("total map count " + str(total_maps) + "\n")
+file.write("avg levels per user " + str(round(total_maps/total_player)) + "\n")
+file.write(str(a) + "\n")
+file.write("______________________________________" + "\n")
+file.close()
+
+# Closing the file
+f.close()
     total_player += 1
     total_maps += value
     file = open('logs.txt', 'a+')
